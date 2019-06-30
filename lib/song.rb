005dd1479c3088ad370985280b91351fb5e15b37
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-30 20:04:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,3 +38,15 @@ end
     @@artists.uniq!
   end
 end
+
+def self.artist_count
+  @@artist_count_hash = {}
+  @@artists.each do |artist|
+    if @@artist_count_hash.has_key?(artist)
+      @@artist_count_hash[artist] += 1 
+    else
+      @@artist_count_hash[artist] = 1 
+    end
+  end
+  @@artist_count_hash
+end
