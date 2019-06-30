87e491e6a382e19499e8beae7c8a3ca82b1d8d9b
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-30 20:00:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,14 +23,14 @@ class Song
   end
   
   def self.genre_count
-    @@g_hash = {}
-    if @@g_hash.has_key?(genre)
-      @@g_hash[genre] += 1 
+    @@genre_count_hash = {}
+    if @@genre_count_hash.has_key?(genre)
+      @@genre_count_hash[genre] += 1 
     else
-      @@g_hash[genre] = 1 
+      @@genre_count_hash[genre] = 1 
     end
   end
-  @@g_hash
+  @@genre_count_hash
 end
       
   def self.artists
