72cc91888a22ad33281177ff39c6762340a37068
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-30 19:43:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,13 +2,14 @@ class Song
   attr_accessor :name, :artist, :genre 
   
   @@count = 0 
-  @@genres = {}
+  @@genres = []
     
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+    @@genres = 
   end
   
   def self.count
